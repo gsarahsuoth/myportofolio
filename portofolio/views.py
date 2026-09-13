569eab090be463432a9d354781cabfc93aa49d92
Gavrila Sarah <gsarahsuoth@gmail.com>
2026-09-13 23:37:24 +0700

    feat: creating a view that retrieves data from the model, places it in a context, and passes it to a new template

--- portofolio/views.py
+++ portofolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from main.models import Experience
+from main.models import Education
 
 
 def show_main(request):
@@ -21,3 +22,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Gavrila Sarah Kartika Suoth",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
