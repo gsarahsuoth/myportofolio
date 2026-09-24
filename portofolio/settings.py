bcdf68f98ba4847c365e38540a4380bc01f0290e
Gavrila Sarah <gsarahsuoth@gmail.com>
2026-09-24 23:13:00 +0700

    fix: fixing pws error (2)

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -156,4 +156,4 @@ MAILERS = {
 }
 
 # gunakan https:// untuk trailing urlnya
-CSRF_TRUSTED_ORIGINS = ["<https://gavrila-sarah-myportofolio.pws.cs.ui.ac.id>"]
+CSRF_TRUSTED_ORIGINS = ["https://gavrila-sarah-myportofolio.pws.cs.ui.ac.id/"]
